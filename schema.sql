cb3f9c2c20f2ca2dabdcf40065506e30a497696f
rdontikurthi <rdontikurthi@binghamton.edu>
2026-06-08 22:50:59 -0400

    add NOT NULL and CHECK constraints

--- schema.sql
+++ schema.sql
@@ -5,10 +5,12 @@ CREATE TABLE users (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE jobs(
+CREATE TABLE jobs (
     id SERIAL PRIMARY KEY,
-    title TEXT NOT NULL,
-    company TEXT NOT NULL,
+    title TEXT NOT NULL CHECK (char_length(title) > 0),
+    company TEXT NOT NULL CHECK (char_length(company) > 0),
+    user_id INT NOT NULL REFERENCES users(id),
+    deleted_at TIMESTAMP DEFAULT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
