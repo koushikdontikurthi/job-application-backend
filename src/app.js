7066c937d547b3f1307030b13013400ca1fd78ab
rdontikurthi <rdontikurthi@binghamton.edu>
2026-06-14 22:59:01 -0400

    add request timeout and rate limiting

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require("express");
-const app = express();
+const timeout = require('connect-timeout');
+const rateLimit = require('express-rate-limit');
+
 
 const {requestLogger} = require("./middlewares/requestLogger");
 const errorHandler = require("./middlewares/errorHandler");
@@ -8,8 +10,20 @@ const authRoutes = require("./routes/authRoutes");
 const jobRoutes = require("./routes/jobRoutes");
 const applicationRoutes = require("./routes/applicationRoutes");
 
+const app = express();
 
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, 
+    max: 100, // max 100 requests per IP per window
+    message: {
+        code: 'RATE_LIMIT_EXCEEDED',
+        message: 'Too many requests, please try again later.'
+    }
+});
+
+app.use(limiter);
 app.use(express.json());
+app.use(timeout('5s'));
 app.use(requestLogger);
 app.get('/health', (req, res) => {
     res.json({ ok: true, message: 'server is running' });
